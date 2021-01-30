cb9fd6eb8a2cd95aeca7988349007a025c0c5b97
jjbreezy <jordanbroihier@gmail.com>
2021-01-30 09:42:40 -0800

    expanded test card array
    and also put the number of cards drawn as a console log statement. gonna experiment with it next to see what I could do with a smaller internal counter.

--- cardgamejs.js
+++ cardgamejs.js
@@ -1,4 +1,6 @@
 /*ensures everything happens AFTER page load*/
+//really have a strange suspicion that doing all of my JS inside this will cause some kind of issue but I don't know what 
+//that issue might be, only that async functions are valuable for JS soooo 
         window.onload = function() {
             console.log('page loaded');
 
@@ -20,20 +22,40 @@
             /*button click for new card*/
             NCB.addEventListener('click', cardFunction);
             /*new card draw function*/
-            function cardFunction() {
+            function cardFunction() { //should probably take inputs
                 //alert('NEW CARD');
                 console.log('card function triggered');
+                //console.log(deck);
+                //local variable for randomly drawn card 
+                let randomlydrawncard = deck[Math.floor(Math.random() * deck.length)];
+                //log the card
+                console.log(randomlydrawncard);
+                document.getElementById("card_display").textContent = randomlydrawncard[0];
             }
+            //card object would be nice, to populate more easily but for right now I think I will just use three separate arrays
+            let card1 = ['Do a silly dance','G','Original','Dare',true];
+            let card2 = ['Pick up your phone','PG-13','Expansion 1','Group', false];
+            let card3 = ['Move your piece two spaces','PG-13','Expansion 2','Movement', false];
+            let card4 = ['4'];
+            let card5 = ['5'];
+            let card6 = ['6'];
+            let card7 = ['7'];
+            let card8 = ['8'];
+            let card9 = ['9'];
+            let card10 = ['10'];
 
-            // non function variable to display the number of cards drawn
+            //pretend deck array, solves a little more of my problem so I could just populate a card from this array. 
+            let deck = [card1,card2,card3,card4,card5,card6,card7,card8,card9,card10];
+
+            //variable to display the number of cards drawn
             var cardsdrawn = 0;
 
             NCB.addEventListener('click', drawCard);
-            //random card
+            //increases the number of cards drawn by one per each card drawn.
             function drawCard() {
                 cardsdrawn = cardsdrawn + 1
-                document.getElementById("card_display").textContent = cardsdrawn + " card drawn";
-                console.log('card drawn successfully');
+                //document.getElementById("card_display").textContent = cardsdrawn + " card drawn";
+                console.log('card drawn successfully ' + cardsdrawn);
             }
 
             /*button click for roll dice*/
@@ -44,6 +66,7 @@
                 console.log('dice roll button pressed');
             }
             
+            //displays the number rolled by the simple dice. first fully functional game element on my site... kinda proud
             RDB.addEventListener('click', diceTalk);
             function diceTalk() {
                 var result = 0;
@@ -51,9 +74,11 @@
                 document.getElementById('dice_display').textContent = result;
                 console.log('number returned = ' + result);
             }
-            //var data;
+            
             const cardsfilepath = '/test_cards.csv'
-
+            //attempt at using papaparse... it returns as the function should per the documentation, but 
+            //is not registering the actual contents of the test CSV or any other CSV I test it on. 
+            //I should probably do this with JSON and populate an array or something
             Papa.parse(cardsfilepath, {
                 delimiter:"",
                 newline:"",
