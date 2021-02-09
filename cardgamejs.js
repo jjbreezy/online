85c28ec1fb4d964807a4a7a2e23e56add01d4da8
jjbreezy <jordanbroihier@gmail.com>
2021-02-08 16:27:26 -0800

    VERSION 1.0
    IT WORKS - took out the test deck, works straight from the JSON

--- cardgamejs.js
+++ cardgamejs.js
@@ -18,49 +18,30 @@
             var NCB = document.getElementById('newCardButton');
             var RDB = document.getElementById('rollDiceButton');
             
-            
-            /*button click for new card*/
-            NCB.addEventListener('click', cardFunction);
-            /*new card draw function*/
-            function cardFunction() { //should probably take inputs
-                //alert('NEW CARD');
-                console.log('card function triggered');
-                //console.log(deck);
-                //local variable for randomly drawn card 
-                let randomlydrawncard = deck[Math.floor(Math.random() * deck.length)];
-                //log the card
-                console.log(randomlydrawncard);
-                document.getElementById("card_display").textContent = randomlydrawncard[0];
-            }
-            //card object would be nice, to populate more easily but for right now I think I will just use three separate arrays
-            let card1 = ['Do a silly dance','G','Original','Dare',true];
-            let card2 = ['Pick up your phone','PG-13','Expansion 1','Group', false];
-            let card3 = ['Move your piece two spaces','R','Expansion 2','Movement', false];
-            let card4 = ['card 4'];
-            let card5 = ['card 5'];
-            let card6 = ['card 6'];
-            let card7 = ['card 7'];
-            let card8 = ['card 8'];
-            let card9 = ['card 9'];
-            let card10 = ['card 10'];
-
-            //pretend deck array, solves a little more of my problem so I could just populate a card from this array. 
-            let deck = [card1,card2,card3,card4,card5,card6,card7,card8,card9,card10];
-
-            //variable to display the number of cards drawn
-            var cardsdrawn = 0;
-
             var jsontestdeck = [];
-            //JSON testing
+            //JSON test deck fetch
             fetch("test_cards.json")
                 .then(response => response.json())
                 .then(data => {
                     jsontestdeck = data;
                     console.log(jsontestdeck);
                 });
-            
-            console.log(jsontestdeck[1] + " testing of global with array of length" + jsontestdeck.length);
-            
+
+            /*button click for new card*/
+            NCB.addEventListener('click', cardFunction);
+            /*new card draw function*/
+            function cardFunction() {
+                console.log('card function triggered');
+                //local variable for randomly drawn card 
+                let randomlydrawncard = jsontestdeck[Math.floor(Math.random() * jsontestdeck.length)];
+                //log the card to ensure it's all good
+                console.log(randomlydrawncard);
+                document.getElementById("card_display").textContent = JSON.stringify(randomlydrawncard.cardcontents);
+            }
+
+            //variable to display the number of cards drawn
+            var cardsdrawn = 0;
+
             NCB.addEventListener('click', drawCard);
             
             //increases the number of cards drawn by one per each card drawn.
@@ -68,8 +49,6 @@
                 cardsdrawn = cardsdrawn + 1
                 console.log('card drawn successfully ' + cardsdrawn);
                 document.getElementById("drawn_counter").textContent = cardsdrawn + " drawn";
-                //returns the card contents
-                console.log(JSON.stringify(jsontestdeck[1].cardcontents) + " jsontestdeck");
             }
 
             //var for number of times rolled
@@ -79,7 +58,6 @@
             RDB.addEventListener('click', rollDice);
             /*roll dice function*/
             function rollDice() {
-                //alert('DICE ROLL');
                 console.log('dice roll button pressed');
                 dicerolled = dicerolled + 1;
                 document.getElementById("rolled_counter").textContent = dicerolled + " rolled";
